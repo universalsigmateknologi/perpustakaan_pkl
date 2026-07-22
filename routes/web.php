fa61c53dc5ccfe3f7913f9fca28984e4f8dd6dd0
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-07-22 11:18:52 +0700

    good

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.dashboard');
 })->name('dashboard');
